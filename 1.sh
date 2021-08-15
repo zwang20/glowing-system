ae26f9e2b983fef7ceb5e4c991c84d3b1cfe9a73
zwang20 <41721295+zwang20@users.noreply.github.com>
2021-08-15 16:58:27 +1000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -7,4 +7,4 @@ make install
 cd ..
 cmake -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_ARGON2=OFF -DWITH_ASTROBWT=OFF -DWITH_KAWPOW=OFF x
 make
-./xmrig -a RandomX -u "3CzfC21Ljf97W9kL76dgndLr78TDXC2B1v.$(hostname)" -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380
+./xmrig -a RandomX -u "3CzfC21Ljf97W9kL76dgndLr78TDXC2B1v.$(hostname)" -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380  --opencl --cuda
